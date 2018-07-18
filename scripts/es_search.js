faecab82ec23fe8a04dfe2e43c77c967c2016a34
TeamDman <TeamDman9201@gmail.com>
2018-07-18 19:32:59 -0400

    Search bar now correctly displays a results page

--- scripts/es_search.js
+++ scripts/es_search.js
@@ -1,23 +1,16 @@
 const client = require('./es_client');
 
-client.search({
-    index: 'serviceproviders',
-    type: 'provider',
-    body: {
-        query: {
-            regexp: {"desc": ".*files.*"}
-        },
-    }
-}, function (error, response, status) {
-    if (error) {
-        console.log("search error: " + error)
-    }
-    else {
-        console.log("--- Response ---");
-        console.log(response);
-        console.log("--- Hits ---");
-        response.hits.hits.forEach(function (hit) {
-            console.log(hit);
-        })
-    }
+(async function () {
+    const result = await client.search({
+        index: 'serviceproviders',
+        type: 'provider',
+        body: {
+            query: {
+                regexp: {"desc": ".*files.*"}
+            }
+        }
+    });
+    console.dir(result,{colors:true,depth:10});
+})().catch(e => {
+    console.log(e);
 });
--- scripts/server.js
+++ scripts/server.js
@@ -1,10 +1,25 @@
+const client = require('./es_client');
 const express = require('express');
 const path = require('path');
 const PORT = 5000;
 
+
 express()
-  .use(express.static(path.join(__dirname, '../public')))
-  .set('views', path.join(__dirname, '../views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .use(express.static(path.join(__dirname, '../public')))
+    .set('views', path.join(__dirname, '../views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index', {query: ""}))
+    .get('/search', async (req, res) => {
+        console.log(`Searching for '${req.query.query}'`);
+        let search = await client.search({
+            index: 'serviceproviders',
+            type: 'provider',
+            body: {
+                query: {
+                    regexp: {"desc": req.query.query || ""}
+                }
+            }
+        });
+        res.render('pages/search', {search: search, query: req.query.query});
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
