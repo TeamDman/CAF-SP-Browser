ee46a0c238bd70375f1549f9aca46d5f66e4a687
TeamDman <TeamDman9201@gmail.com>
2018-07-24 20:14:43 -0400

    Move curl to compose.yml

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@ FROM node:8
 WORKDIR /app
 COPY . /app
 
-RUN curl https://caf-shib2ops.ca/CoreServices/caf_interfed_signed.xml -o resources/caf_interfed_signed.xml
-
 RUN npm install
 
 RUN chmod +x /app/install.sh
--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ until $(curl --output /dev/null --silent --head --fail elasticsearch:9200); do
   printf '.'
   sleep 5
 done
+curl https://caf-shib2ops.ca/CoreServices/caf_interfed_signed.xml -o resources/caf_interfed_signed.xml
+
 npm run setup
