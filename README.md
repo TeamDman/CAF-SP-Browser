f12e10d9d5c8712b20f707cd92f52b1f3da99c2a
TeamDman <TeamDman9201@gmail.com>
2019-09-21 02:08:20 +0100

    Cleanup and fresh start

--- README.md
+++
@@ -1,5 +0,0 @@
-# CAF-SP-Browser
-A web interface prototype to browse Service Providers with relations to the Canadian Access Federation
-
-
-Run `docker-compose up` and visit `localhost:5000` to view the prototype
--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-until $(curl --output /dev/null --silent --head --fail elasticsearch:9200); do
-  printf '.'
-  sleep 5
-done
-curl https://caf-shib2ops.ca/CoreServices/caf_interfed_signed.xml -o resources/caf_interfed_signed.xml
-
-npm run setup
